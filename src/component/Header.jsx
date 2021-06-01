390381afdb5a7d0904bbef4ae8da5948d94c15b7
HyoSung <themu@naver.com>
2021-06-02 01:09:28 +0900

    Pause Page - Pause Logo (Text) - Circle

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
+
+
 function Header() {
-  return (
-    <header className='header'>
-    	<ul>
-    		<li className='active'>Ready</li>
-    		<li>Running</li>
-    		<li>Pause</li>
-    	</ul>
-    </header>
-  );
+  const Nav = () => {
+    let pageClass = {
+      ready: '',
+      running: '',
+      pause: '',
+    };
+    const paths = document.location.href.split('//').pop().split('/');
+    pageClass[paths[1]] += 'active';
+
+    return (
+      <header className='header'>
+        <ul>
+          <li className={pageClass['ready']}><a href='/ready'>Ready</a></li>
+          <li className={pageClass['running']}><a href='/running'>Running</a></li>
+          <li className={pageClass['pause']}><a href="/pause">Pause</a></li>
+        </ul>
+      </header>
+    );
+  };
+  return <Nav />;
 }
 
 export default Header;
--- src/pages/PausePage.js
+++ src/pages/PausePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './PausePage.css';
 
 const PausePage = () => {
   return(
-    <div>
-        <h2>Pause Page</h2>
+    <div id="pause">
+      <div className="speed">
+        <span className="speed-text">||</span>
+      </div>
     </div>
   )
 }
